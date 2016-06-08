1ad1f2686e204991a31ae8f6a3fac4b15e2cc05f
Johnnie Hard <johnnie.hard@gmail.com>
2016-06-08 15:17:40 +0200

    using the mouse as a player dummy for testing

--- map.js
+++ map.js
@@ -2,10 +2,7 @@
 
 
 
-var controlPoint1 = L.latLng(59.309892, 18.075331);
-
-
-//59.309892, 18.075331 //skanstull (gunnarssons)
+var controlPoint1 = L.latLng(59.309892, 18.075331);//59.309892, 18.075331 //skanstull (gunnarssons)
 
 
 var map = L.map("map", {
@@ -23,6 +20,7 @@ var CartoDB_DarkMatter = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/
 
 CartoDB_DarkMatter.addTo(map);
 
+
 //custom control
 var MyControl = L.Control.extend({
     options: {
@@ -39,9 +37,14 @@ var MyControl = L.Control.extend({
 
 map.addControl(new MyControl());
 
+
+
 L.marker(controlPoint1)
     .addTo(map);
 
+
+
+//Geolocation
 map.locate({
     setView: true,
     maxZoom: 16,
@@ -72,3 +75,14 @@ function onLocationError(e) {
 }
 
 map.on('locationerror', onLocationError);
+
+
+//debug mode mouse marker
+var mousemarker = L.circle(map.getCenter(), 10).addTo(map);
+
+map.on('mousemove', function(e){
+    mousemarker.setLatLng(e.latlng);//.update();
+    if(e.latlng.distanceTo(controlPoint1) < 100){
+        alert("Du närmar dig målet!");
+    }
+});
